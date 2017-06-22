a31d4fb14a9655b7fa9f0d926e627f9e606a0d4e
Samantha Morco <samanthamorco@gmail.com>
2017-06-22 18:49:29 -0400

    initial commit

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,11 @@
+/* global Vue */
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!',
+      header: 'Go Gitters',
+      review1: ''
+    }
+  });
+});
