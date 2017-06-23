664b76b353a82d384e518e3cf95e36dfbf16d8a2
Samantha Morco <samanthamorco@gmail.com>
2017-06-22 20:14:05 -0400

    v-if and putting in objects

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -5,8 +5,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
     data: {
       message: 'Hello Vue!',
       header: 'Go Gitters',
-      review1: '',
-      reviews: ['Vue.js is super cool', 'I think this is all bad', 'I do not get the point of this', 'I think everything is cool']
+      newReview: '',
+      reviewerName: '',
+      reviews: [{author: 'Anonymous', text: 'Vue.js is super cool'}, {author: 'Anonymous', text: 'I think this is all bad'}, {author: 'Anonymous', text: 'I do get the point of this'}, {author: 'Anonymous', text: 'I think everything is cool'}]
+    },
+    methods: {
+      addReview: function() {
+        if (this.newReview) {
+          this.reviews.push({text: this.newReview, author: this.reviewerName});
+          this.newReview = "";
+          this.reviewerName = "";
+        } else {
+          alert("Enter something!");
+        }
+      },
+      removeReview: function(review) {
+        var index = this.reviews.indexOf(review);
+        this.reviews.splice(index, 1);
+      },
+      isNotBad: function(review) {
+        return review.text.toLowerCase().indexOf('bad') === -1;
+      }
     }
   });
 });
